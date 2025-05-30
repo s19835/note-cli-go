f3585d26b8661a27afa1be86d4090007de01bf69
s19835 <s19835@sci.pdn.ac.lk>
2025-05-30 19:08:26 +0530

    add: concept command

--- cmd/concept.go
+++ cmd/concept.go
@@ -1,27 +1,48 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
 // conceptCmd represents the concept command
 var conceptCmd = &cobra.Command{
-	Use:   "concept",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "concept [note]",
+	Short: "Add a small concept or unorganized learning",
+	Long: `Purpose: Mini-notes of stuff you learned but haven't fully processed into atomic permanent notes, Messy but still valuable.
+	Title: concept-YYYY-MM.md, Location: notes/concepts/`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("concept called")
+		note := strings.Join(args, " ")
+		now := time.Now()
+
+		filename := fmt.Sprintf("concepts-%d-%02d.md", now.Year(), now.Month())
+		baseDir := "notes/concepts"
+		fullpath := filepath.Join(baseDir, filename)
+
+		os.MkdirAll(baseDir, os.ModePerm)
+
+		content := fmt.Sprintf("\n## %s\n\n- %s\n", now.Format("2006-01-02 15:04"), note)
+
+		f, err := os.OpenFile(fullpath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			fmt.Println("Could not write to concept file: ", err)
+			return
+		}
+		defer f.Close()
+
+		if _, err := f.WriteString(content); err != nil {
+			fmt.Println("Error writing to file: ", err)
+		} else {
+			fmt.Println("Creates ", fullpath)
+		}
 	},
 }
 
