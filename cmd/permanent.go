07ba9f3d978421782d996dac1b5c19542c13114c
s19835 <s19835@sci.pdn.ac.lk>
2025-05-30 20:53:44 +0530

    add: permanent note

--- cmd/permanent.go
+++ cmd/permanent.go
@@ -1,27 +1,67 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 )
 
 // permanentCmd represents the permanent command
 var permanentCmd = &cobra.Command{
-	Use:   "permanent",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Use:   "permanent [note]",
+	Short: "Create Permanent note with a structured format",
+	Long: `Fully processed ideas or concepts, backed by understanding, usually written in you own words. These are atomic, connected, and resuable.
+	Title: Short descriptive title, Location: notes/permanent/`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("permanent called")
+		title := strings.Join(args, " ")
+		slug := slugify(title)
+		filename := slug + ".md"
+
+		baseDir := "notes/permanent"
+		fullpath := filepath.Join(baseDir, filename)
+
+		os.MkdirAll(baseDir, os.ModePerm)
+
+		created := time.Now().Format("2006-01-02 15:04")
+		content := fmt.Sprintf(`# %s
+
+**created:** %s
+
+## Summary
+
+> One-linear explanation.
+
+## Context
+
+Explain where you learned it, why it matters.
+
+## Details
+
+Explain the concept, give examples, diagrams, etc.
+
+## Related Notes
+
+relevent permanent notes
+
+## Tags
+
+[tag1] [tag2] [concept]
+`, title, created)
+
+		err := os.WriteFile(fullpath, []byte(content), 0644)
+		if err != nil {
+			fmt.Println("could not created permanant note")
+			return
+		}
+
+		fmt.Println("Creates ", fullpath)
 	},
 }
 
@@ -37,4 +77,11 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// permanentCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func slugify(s string) string {
+	s = strings.ToLower(s)
+	s = strings.ReplaceAll(s, " ", "_")
+	s = strings.ReplaceAll(s, "/", "_")
+	return s
 }
